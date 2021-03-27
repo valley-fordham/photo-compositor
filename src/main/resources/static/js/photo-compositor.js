09e3733719058add5e10be31e310e3c54bfd20f1
valley <valley@noreply.com>
2021-03-27 18:38:10 +1030

    Bug Fixes
    - fixed scaling of photo
    - workaround to avoid Chrome freezing
    - display an error if no camera available or no camera permissions

--- src/main/resources/static/js/photo-compositor.js
+++ src/main/resources/static/js/photo-compositor.js
@@ -6,21 +6,25 @@ const context = photoCanvas.getContext('2d');
 const imgRiot = document.getElementById('img-riot');
 const btnCapture = document.getElementById('btn-capture');
 const btnRetry = document.getElementById('btn-retry');
+const cameraWarning = document.getElementById('camera-warning');
+let cameraX = 0;
+let cameraY = 0;
 
 if (supported) {
 	showCameraView();
 } else {
-	console.log('camera not supported');
+	cameraWarning.style.display = "block";
 }
 
 function takePhoto()  {
 	context.drawImage(cameraView, 0, 0, photoCanvas.width, photoCanvas.height);
-	context.drawImage(imgRiot, 0, 160, imgRiot.width, imgRiot.height);
+	context.drawImage(imgRiot, 0, 400, imgRiot.width, imgRiot.height);
 	hideCameraView();
 }
 
 function hideCameraView() {
-	cameraView.srcObject.getVideoTracks().forEach(track => track.stop());
+	// this is broken in current version of Chrome, may be able to bring it back later
+	// cameraView.srcObject.getVideoTracks().forEach(track => track.stop());
 	cameraView.style.display = "none";
 	photoCanvas.style.display = "block";
 	btnCapture.style.display = "none";
@@ -40,6 +44,12 @@ function showCameraView() {
 	navigator.mediaDevices.getUserMedia(constraints)
 		.then((stream) => {
 			cameraView.srcObject = stream;
+			cameraX = stream.getVideoTracks()[0].getSettings().width;
+			cameraY = stream.getVideoTracks()[0].getSettings().height;
+			console.log(cameraX);
+			console.log(cameraY);
 			document.getElementById('btn-capture').style.display = "block";
-		});
+		}).catch(function() {
+			cameraWarning.style.display = "block";
+	});
 }
