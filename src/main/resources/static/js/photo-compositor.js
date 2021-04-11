01c54b0494b5b1a5be2f10d18ff7f0aee837c2b1
valley <valley@noreply.com>
2021-04-11 16:13:13 +0930

    General development
    - added ability to change overlay option
    - added headless riot overlay option

--- src/main/resources/static/js/photo-compositor.js
+++ src/main/resources/static/js/photo-compositor.js
@@ -3,17 +3,35 @@ const supported = 'mediaDevices' in navigator;
 const cameraView = document.getElementById('camera');
 const photoCanvas = document.getElementById('canvas');
 const context = photoCanvas.getContext('2d');
-const imgRiot = document.getElementById('img-riot');
+const imgFuturama = document.getElementById('img-futurama');
+const imgHeadless = document.getElementById('img-headless');
 const photoTitle = document.getElementById('img-photo-title');
 const btnCapture = document.getElementById('btn-capture');
+const btnChangeOverlay = document.getElementById('btn-change-overlay');
+const imgSpeechBubble = document.getElementById('img-speech-bubble');
 const btnRetry = document.getElementById('btn-retry');
 const btnDownload = document.getElementById('btn-download');
 const txtCameraNotSupported = document.getElementById('camera-not-supported');
 const txtCameraNoPermission = document.getElementById('camera-no-permission');
 const txtLandscapeNotSupported = document.getElementById('landscape-not-supported');
+const imgBackground = document.getElementById('img-background');
+
+const OVERLAY = Object.freeze({"futurama": 0, "headless": 1});
+
 let isPhotoDisplayed = false;
+let currentOverlay = OVERLAY.futurama;
 let cameraX = 0;
 let cameraY = 0;
+
+
+// Initialise screen
+if (supported) {
+	showCameraView();
+} else {
+	txtCameraNotSupported.style.display = "block";
+}
+setOverlay(OVERLAY.futurama);
+
 
 window.addEventListener("orientationchange", function() {
 	checkOrientation();
@@ -27,6 +45,8 @@ function checkOrientation() {
 		btnCapture.style.display = "none";
 		btnDownload.style.display = "none";
 		btnRetry.style.display = "none";
+		btnChangeOverlay.style.display = "none";
+		imgSpeechBubble.style.display = "none";
 		txtLandscapeNotSupported.style.display = "block";
 	} else if (supported) {
 		if (isPhotoDisplayed) {
@@ -36,20 +56,16 @@ function checkOrientation() {
 		} else {
 			cameraView.style.display = "block";
 			btnCapture.style.display = "block";
+			btnChangeOverlay.style.display = "block";
+			imgSpeechBubble.style.display = "block";
 		}
 		txtLandscapeNotSupported.style.display = "none";
 	}
 }
 
-if (supported) {
-	showCameraView();
-} else {
-	txtCameraNotSupported.style.display = "block";
-}
-
 function takePhoto()  {	
 	context.drawImage(cameraView, 0, 0, photoCanvas.width, photoCanvas.height);
-	context.drawImage(imgRiot, 0, 400, imgRiot.width, imgRiot.height);
+	drawCompositeOverlay();
 	context.drawImage(photoTitle, 0, 20, photoTitle.width, photoTitle.height);
 	hideCameraView();
 }
@@ -60,8 +76,11 @@ function hideCameraView() {
 		cameraView.srcObject.getVideoTracks().forEach(track => track.stop());
 	}
 	cameraView.style.display = "none";
+	imgBackground.style.display = "none";
 	photoCanvas.style.display = "block";
 	btnCapture.style.display = "none";
+	btnChangeOverlay.style.display = "none";
+	imgSpeechBubble.style.display = "none";
 	btnRetry.style.display = "block";
 	btnDownload.style.display = "block";
 	isPhotoDisplayed = true;
@@ -72,6 +91,7 @@ function showCameraView() {
 	photoCanvas.style.display = "none";
 	btnRetry.style.display = "none";
 	btnDownload.style.display = "none";
+	imgBackground.style.display = "block";
 	
 	const constraints = {
 		video: true,
@@ -86,6 +106,8 @@ function showCameraView() {
 			cameraY = stream.getVideoTracks()[0].getSettings().height;
 			cameraView.style.display = "block";
 			btnCapture.style.display = "block";
+			btnChangeOverlay.style.display = "block";
+			imgSpeechBubble.style.display = "block";
 			checkOrientation();
 		}).catch(function() {
 			txtCameraNoPermission.style.display = "block";
@@ -97,4 +119,36 @@ function downloadPhoto(){
 	photoCanvas.toBlob(function(blob) {
 		saveAs(blob, "riot-" + new Date() + ".png");
 	});
+}
+
+function changeOverlay() {
+	currentOverlay++;
+	// Wrap Overlay when cycling between options
+	if (currentOverlay > OVERLAY.headless) {
+		currentOverlay = OVERLAY.futurama;
+	}
+	setOverlay(currentOverlay);
+}
+
+function setOverlay(overlay) {
+	switch (overlay) {
+		case OVERLAY.futurama:
+			imgBackground.src="images/composite-futurama.png";
+			break;
+		case OVERLAY.headless:
+			imgBackground.src="images/composite-headless.png";
+			break;
+		default:
+			console.log('unsupported overlay');
+	}
+}
+
+function drawCompositeOverlay() {
+	switch (currentOverlay) {
+		case OVERLAY.futurama:
+			context.drawImage(imgFuturama, 0, 400, imgFuturama.width, imgFuturama.height);
+			break;
+		case OVERLAY.headless:
+			context.drawImage(imgHeadless, 0, 240, imgHeadless.width, imgHeadless.height);
+	}
 }
